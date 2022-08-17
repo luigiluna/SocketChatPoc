4b17a4aea0de61c2865b5f487dc207c8f03d3163
sales26 <sales26@avaya.com>
2022-08-17 17:15:27 -0300

    First

---
+++ public/main.js
@@ -0,0 +1,193 @@
+const socket = io();
+let username = '';
+let userList = [];
+
+let loginPage = document.querySelector('#loginPage');
+let chatPage = document.querySelector('#chatPage');
+let content = document.querySelectorAll('.content');
+
+let loginInput = document.querySelector('#loginNameInput');
+let textInput = document.querySelector('#chatTextInput');
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+content[0].style.display = 'none';
+content[1].style.display = 'none';
+
+let separator = "╡§";
+
+function getOldMsgs(){
+    let msgs = sessionStorage.getItem("chat");
+
+    if(msgs!=null){
+        let msgArr = msgs.split(separator);
+
+        console.log(msgArr);
+
+        msgArr.forEach((ele)=>{
+            if(ele!=" " && ele!=""){
+                let userAndMsg = ele.split(":");
+                addMessage('msg', userAndMsg[0], userAndMsg[1]);
+            }
+        })
+    }
+   
+
+}
+
+function checkUser(){
+    let user = sessionStorage.getItem('user');
+    if(user != null){
+        username = user;
+        document.title = 'Chat ('+username+')';
+        socket.emit('join-request-recon', username);
+        loginPage.style.display = 'none';
+        chatPage.style.display = 'flex';
+        content[0].style.display = 'inline';
+        content[1].style.display = 'inline';
+        textInput.focus();
+    } 
+}
+
+function addStorageItem(user,msg){
+
+    msg = msg.replace(separator,"").trim();
+    user = user.replace(separator,"").trim();
+    
+    let oldMsgs = sessionStorage.getItem("chat");
+
+    if(oldMsgs != null){
+        oldMsgs = oldMsgs + `${user}:${msg}${separator}`
+    } else {
+        oldMsgs =`${user}:${msg}${separator}`
+    }
+
+    sessionStorage.setItem("chat", oldMsgs);
+
+}
+
+function renderUserList() {
+    let ul = document.querySelector('.userList');
+    ul.innerHTML = '';
+
+    userList.forEach(i => {
+        ul.innerHTML += '<li>'+i+'</li>';
+    });
+}
+
+function addMessage(type, user, msg) {
+    let ul = document.querySelector('.chatList');
+
+    switch(type) {
+        case 'status':
+            ul.innerHTML += '<li class="m-status">'+msg+'</li>';
+        break;
+        case 'msg':
+            if(username == user) {
+                ul.innerHTML += '<li class="m-txt"><span class="me">'+user+':'+'</span> '+msg+'</li>';
+            } else {
+                ul.innerHTML += '<li class="m-txt"><span>'+user+':'+'</span> '+msg+'</li>';
+            }
+        break;
+    }
+
+    ul.scrollTop = ul.scrollHeight;
+}
+
+
+loginInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+        let name = loginInput.value.trim();
+        if(name != '') {
+            username = name;
+            document.title = 'Chat ('+username+')';
+            sessionStorage.setItem('user', username);
+            socket.emit('join-request', username);
+        }
+    }
+});
+
+textInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+        let txt = textInput.value.trim();
+        textInput.value = '';
+
+        if(txt != '') {
+            addStorageItem(username,txt);
+            addMessage('msg', username, txt);
+            socket.emit('send-msg', txt);
+        }
+    }
+});
+
+socket.on('user-ok', (list) => {
+    loginPage.style.display = 'none';
+    chatPage.style.display = 'flex';
+    content[0].style.display = 'inline';
+    content[1].style.display = 'inline';
+    textInput.focus();
+
+    addMessage('status', null, 'Conectado!');
+
+    userList = list;
+    renderUserList();
+});
+
+socket.on('user-ok-recon', (list) => {
+    loginPage.style.display = 'none';
+    chatPage.style.display = 'flex';
+    content[0].style.display = 'inline';
+    content[1].style.display = 'inline';
+
+    textInput.focus();
+
+   
+
+    userList = list;
+    renderUserList();
+});
+
+socket.on('list-update', (data) => {
+    if(data.joined) {
+        addMessage('status', null, data.joined+' entrou no chat.');
+    }
+
+    if(data.left) {
+        //addMessage('status', null, data.left+' saiu do chat.');
+    }
+
+    userList = data.list;
+    renderUserList();
+});
+
+socket.on('list-update-recon', (data) => {
+  
+    userList = data.list;
+    renderUserList();
+});
+
+socket.on('show-msg', (data) => {
+    addStorageItem(data.username, data.message);
+    addMessage('msg', data.username, data.message);
+});
+
+socket.on('disconnect', () => {
+    addMessage('status', null, 'Você foi desconectado!');
+    userList = [];
+    renderUserList();
+});
+
+socket.on('reconnect_error', () => {
+    addMessage('status', null, 'Tentando reconectar...');
+});
+
+socket.on('reconnect', () => {
+    addMessage('status', null, 'Reconectado!');
+
+    if(username != '') {
+        socket.emit('join-request', username);
+    }
+});
+
+
+checkUser();
---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+
+server.listen(3000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set( __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get("/", (req,res)=>{
+    res.render("index");
+
+})
+
+app.get("/A", (req,res)=>{
+    res.render("index");
+
+})
+
+app.get("/B", (req,res)=>{
+    res.render("index B");
+   
+})
+
+
+let connectedUsers = [];
+
+io.on('connection', (socket) => {
+    console.log("Conexão detectada...");
+
+    socket.on('join-request', (username) => {
+        socket.username = username;
+        connectedUsers.push( username );
+        console.log( connectedUsers );
+
+        socket.emit('user-ok', connectedUsers);
+        socket.broadcast.emit('list-update', {
+            joined: username,
+            list: connectedUsers
+        });
+    });
+
+    socket.on('join-request-recon', (username) => {
+        socket.username = username;
+        connectedUsers.push( username );
+        console.log( connectedUsers );
+
+        socket.emit('user-ok-recon', connectedUsers);
+        socket.broadcast.emit('list-update-recon', {
+            joined: username,
+            list: connectedUsers
+        });
+        
+    });
+
+    socket.on('disconnect', () => {
+        connectedUsers = connectedUsers.filter(u => u != socket.username);
+        console.log(connectedUsers);
+
+        socket.broadcast.emit('list-update-recon', {
+            left: socket.username,
+            list: connectedUsers
+        });
+
+    });
+
+    socket.on('send-msg', (txt) => {
+        let obj = {
+            username: socket.username,
+            message: txt
+        };
+
+        //socket.emit('show-msg', obj);
+        socket.broadcast.emit('show-msg', obj);
+    });
+
+
+    socket.on('close', () => {
+        connectedUsers = connectedUsers.filter(u => u != socket.username);
+        console.log(connectedUsers);
+
+        socket.broadcast.emit('list-update', {
+            left: socket.username,
+            list: connectedUsers
+        });
+
+    });
+
+    
+});
